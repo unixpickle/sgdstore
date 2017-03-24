7ef5b4546e668f584351c28a4769362068df9883
Alex Nichol <unixpickle@gmail.com>
2017-03-24 15:35:38 -0400

    virtual back-propagation works

--- net.go
+++ net.go
@@ -8,8 +8,7 @@ import (
 
 // Net is a dynamic feed-forward multi-layer perceptron.
 //
-// Each layer, except for the output layer, is implicitly
-// followed by a tanh activation.
+// Each layer is implicitly followed by a tanh.
 type Net struct {
 	// Parameters stores the weights and biases of the
 	// network.
@@ -27,22 +26,106 @@ func (n *Net) Apply(inBatch anydiff.Res, batchSize int) anydiff.Res {
 			panic("mismatching bias and weight count")
 		}
 		for i := 0; i < len(params); i += 2 {
-			weights := params[i]
-			biases := params[i+1]
-			if i > 0 {
-				inBatch = anydiff.Tanh(inBatch)
-			}
-			outSize := biases.Output().Len()
-			inSize := weights.Output().Len() / outSize
-			if inSize*batchSize != inBatch.Output().Len() {
-				panic(fmt.Sprintf("layer %d: input size %d should be %d", i,
-					inBatch.Output().Len()/batchSize, inSize))
-			}
-			inMat := &anydiff.Matrix{Data: inBatch, Rows: batchSize, Cols: inSize}
-			weightMat := &anydiff.Matrix{Data: weights, Rows: outSize, Cols: inSize}
-			inBatch = anydiff.MatMul(false, true, inMat, weightMat).Data
-			inBatch = anydiff.AddRepeated(inBatch, biases)
+			inBatch = applyLayer(params[i], params[i+1], inBatch, batchSize)
 		}
 		return inBatch
+	})
+}
+
+// Train performs SGD training on the batch.
+//
+// The input, target, and stepSize needn't be pooled by
+// the caller.
+func (n *Net) Train(inBatch, target, stepSize anydiff.Res, batchSize,
+	numSteps int) *Net {
+	ins := anydiff.Fuse(inBatch, target, stepSize)
+	newParams := anydiff.PoolMulti(ins, func(s []anydiff.Res) anydiff.MultiRes {
+		inBatch, target, stepSize := s[0], s[1], s[2]
+		net := n
+		for i := 0; i < numSteps; i++ {
+			net = net.step(inBatch, target, stepSize, batchSize)
+		}
+		return net.Parameters
+	})
+	return &Net{Parameters: newParams}
+}
+
+// step performs a step of gradient descent and returns
+// the new network.
+//
+// The input, target, and stepSize should be pooled by the
+// caller.
+func (n *Net) step(inBatch, target, stepSize anydiff.Res, batchSize int) *Net {
+	newParams := anydiff.PoolMulti(n.Parameters, func(params []anydiff.Res) anydiff.MultiRes {
+		grad := applyBackprop(params, inBatch, target, batchSize)
+		return anydiff.PoolMulti(grad, func(grads []anydiff.Res) anydiff.MultiRes {
+			var newParams []anydiff.Res
+			for i, g := range grads[1:] {
+				p := anydiff.Add(params[i], anydiff.ScaleRepeated(g, stepSize))
+				newParams = append(newParams, p)
+			}
+			return anydiff.Fuse(newParams...)
+		})
+	})
+	return &Net{Parameters: newParams}
+}
+
+// applyLayer applies a single layer.
+func applyLayer(weights, biases, inBatch anydiff.Res, batchSize int) anydiff.Res {
+	outSize := biases.Output().Len()
+	inSize := weights.Output().Len() / outSize
+	if inSize*batchSize != inBatch.Output().Len() {
+		panic(fmt.Sprintf("input size %d should be %d",
+			inBatch.Output().Len()/batchSize, inSize))
+	}
+	inMat := &anydiff.Matrix{Data: inBatch, Rows: batchSize, Cols: inSize}
+	weightMat := &anydiff.Matrix{Data: weights, Rows: outSize, Cols: inSize}
+	inBatch = anydiff.MatMul(false, true, inMat, weightMat).Data
+	return anydiff.Tanh(anydiff.AddRepeated(inBatch, biases))
+}
+
+// applyBackprop applies the network and performs
+// backward-propagation.
+// The result is [inGrad, param1Grad, param2Grad, ...].
+// The caller should pool the input parameters.
+func applyBackprop(params []anydiff.Res, in, target anydiff.Res,
+	batchSize int) anydiff.MultiRes {
+	if len(params) == 0 {
+		scaler := target.Output().Creator().MakeNumeric(
+			2 / float64(target.Output().Len()),
+		)
+		return anydiff.Fuse(anydiff.Scale(anydiff.Sub(target, in), scaler))
+	}
+	weights, biases := params[0], params[1]
+	outSize := biases.Output().Len()
+	inSize := weights.Output().Len() / outSize
+	if inSize*batchSize != in.Output().Len() {
+		panic(fmt.Sprintf("input size %d should be %d",
+			in.Output().Len()/batchSize, inSize))
+	}
+	inMat := &anydiff.Matrix{Data: in, Rows: batchSize, Cols: inSize}
+	weightMat := &anydiff.Matrix{Data: weights, Rows: outSize, Cols: inSize}
+	matOut := anydiff.MatMul(false, true, inMat, weightMat).Data
+	biasOut := anydiff.AddRepeated(matOut, biases)
+	tanhOut := anydiff.Tanh(biasOut)
+	return anydiff.PoolFork(tanhOut, func(tanhOut anydiff.Res) anydiff.MultiRes {
+		nextOut := applyBackprop(params[2:], tanhOut, target, batchSize)
+		return anydiff.PoolMulti(nextOut, func(x []anydiff.Res) anydiff.MultiRes {
+			outGrad := x[0]
+			laterGrads := x[1:]
+			pg := anydiff.Mul(anydiff.Complement(anydiff.Square(tanhOut)), outGrad)
+			return anydiff.PoolFork(pg, func(pg anydiff.Res) anydiff.MultiRes {
+				productGrad := &anydiff.Matrix{
+					Data: pg,
+					Rows: batchSize,
+					Cols: outSize,
+				}
+				weightGrad := anydiff.MatMul(true, false, productGrad, inMat).Data
+				biasGrad := anydiff.SumRows(productGrad)
+				inGrad := anydiff.MatMul(false, false, productGrad, weightMat).Data
+				ourGrad := []anydiff.Res{inGrad, weightGrad, biasGrad}
+				return anydiff.Fuse(append(ourGrad, laterGrads...)...)
+			})
+		})
 	})
 }
--- net_test.go
+++ net_test.go
@@ -11,21 +11,7 @@ import (
 
 func TestNetApply(t *testing.T) {
 	c := anyvec64.CurrentCreator()
-	realNet := anynet.Net{
-		anynet.NewFC(c, 3, 5),
-		anynet.Tanh,
-		anynet.NewFC(c, 5, 4),
-		anynet.Tanh,
-		anynet.NewFC(c, 4, 2),
-	}
-
-	var netParams []anydiff.Res
-	for i := 0; i < len(realNet); i += 2 {
-		fc := realNet[i].(*anynet.FC)
-		anyvec.Rand(fc.Biases.Vector, anyvec.Normal, nil)
-		netParams = append(netParams, fc.Weights, fc.Biases)
-	}
-	virtualNet := &Net{Parameters: anydiff.Fuse(netParams...)}
+	realNet, virtualNet := randomNetwork(c)
 
 	inVec := c.MakeVector(12)
 	anyvec.Rand(inVec, anyvec.Normal, nil)
@@ -41,4 +27,60 @@ func TestNetApply(t *testing.T) {
 	if maxDiff > 1e-4 {
 		t.Errorf("expected %v but got %v", expectedOut.Data(), actualOut.Data())
 	}
+}
+
+func TestNetTrain(t *testing.T) {
+	c := anyvec64.CurrentCreator()
+	realNet, virtualNet := randomNetwork(c)
+
+	inVec := c.MakeVector(12)
+	anyvec.Rand(inVec, anyvec.Normal, nil)
+	input := anydiff.NewVar(inVec)
+
+	targetVec := c.MakeVector(8)
+	anyvec.Rand(targetVec, anyvec.Normal, nil)
+	target := anydiff.NewVar(targetVec)
+
+	stepSize := c.MakeVector(1)
+	stepSize.AddScaler(c.MakeNumeric(0.1))
+
+	trained := virtualNet.Train(input, target, anydiff.NewConst(stepSize), 4, 2)
+	actual := trained.Parameters.Outputs()
+
+	for i := 0; i < 2; i++ {
+		out := realNet.Apply(input, 4)
+		cost := anynet.MSE{}.Cost(target, out, 1)
+		grad := anydiff.NewGrad(realNet.Parameters()...)
+		cost.Propagate(stepSize.Copy(), grad)
+		grad.Scale(c.MakeNumeric(-1))
+		grad.AddToVars()
+	}
+	for i, a := range actual {
+		x := realNet.Parameters()[i].Vector
+		diff := x.Copy()
+		diff.Sub(a)
+		maxDiff := anyvec.AbsMax(diff).(float64)
+		if maxDiff > 1e-4 {
+			t.Error("bad value for layer", i)
+		}
+	}
+}
+
+func randomNetwork(c anyvec.Creator) (anynet.Net, *Net) {
+	realNet := anynet.Net{
+		anynet.NewFC(c, 3, 5),
+		anynet.Tanh,
+		anynet.NewFC(c, 5, 4),
+		anynet.Tanh,
+		anynet.NewFC(c, 4, 2),
+		anynet.Tanh,
+	}
+	var netParams []anydiff.Res
+	for i, param := range realNet.Parameters() {
+		if i%2 == 0 {
+			anyvec.Rand(param.Vector, anyvec.Normal, nil)
+		}
+		netParams = append(netParams, param)
+	}
+	return realNet, &Net{Parameters: anydiff.Fuse(netParams...)}
 }
